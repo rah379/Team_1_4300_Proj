20ae6fe7da6c219c0245c9dbabf517659ad32579
Kevin Cui <40209975+KevinKCui@users.noreply.github.com>
2024-03-22 15:30:01 -0400

    Update app.py
    changed filepath

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 print(current_directory)
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'experimental/init.json')
+json_file_path = os.path.join(current_directory, 'init.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
