279c2ff2659902b92c1b740e5a0376f1ea8270c1
Kevin Cui <kc734@cornell.edu>
2024-04-13 16:26:56 -0400

    updated file names -- also changed mechanism to use svd cossine similarity

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,8 @@ import json
 import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
-from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
-import pandas as pd
-import re
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from nltk.stem import PorterStemmer
-ps = PorterStemmer()
+import numpy as np
+from helpers.similarity import svd_cos
 # ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
 os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
@@ -17,105 +12,36 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 print(current_directory)
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'init.json')
+json_file_path = os.path.join(current_directory, 'data/json/docs.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
-    tweets_data = json.load(file)
+    docs = json.load(file)
 
-print(tweets_data.keys())
-######
-word_regex = re.compile(r"""
-    (\w+)
-    """, re.VERBOSE)
-
-
-def getwords(sent):
-    return [w.lower()
-            for w in word_regex.findall(sent)]
-
-
-splitter = re.compile(r"""
-     (?<![A-Z])[.!?](?=\s+[A-Z])
-    """, re.VERBOSE)
-
-# list_words = defaultdict(list)
-list_words = []
-key = 0
-for individual in tweets_data.keys():
-    tweets = tweets_data[individual]
-    tweet_lengths = []
-    words = []
-    for i in range(0, len(tweets)):
-        sent_words_lower = [getwords(sent)
-                            for sent in splitter.split(tweets[i]['Content'].replace("#", ""))]
-        sent_words_lower = [
-            item for row in sent_words_lower for item in row if len(item) > 1 and len(item) < 20]
-        # we want real words
-        sent_words_lower = [ps.stem(word) for word in sent_words_lower]
-        words += sent_words_lower
-
-    list_words.append(words)
-    key += 1
-
-
-# print(set_words[1])
-for i in range(0, len(list_words)):
-    list_words[i] = " ".join(list_words[i])
-# print(len(list_words[1]))
-print(len(list_words))
-
-vectorizer = TfidfVectorizer()
-counter = CountVectorizer()
-
-# TD-IDF Matrix
-tfidf = vectorizer.fit_transform(list_words)
-# print("HERE")
-# print(cosine_similarity(tfidf, tfidf))
+wcnt = np.load(
+    os.path.join(current_directory, 'data/numpy/wcn_transpose.npy'))
+dcn = np.load(os.path.join(current_directory, 'data/numpy/dcn.npy'))
+with open(os.path.join(current_directory, 'data/json/index_politicians.json'), 'r') as f:
+    itp = json.load(f)
 
 app = Flask(__name__)
 CORS(app)
 
-# Sample search using json with pandas
-
-
 # maybe we can build out the cosine similarity matrix before? and just load that information in
-
-
-def cossim_search(query):
-    # matches = []
-    list_words.append(query.lower())
-    query = [ps.stem(word) for word in splitter.split(query.lower())]
-    print(query)
-    tfidf = vectorizer.fit_transform(list_words)
-    cossim = cosine_similarity(tfidf, tfidf)
-    print(cossim)
-    relevant = (-cossim[-1]).argsort()[1:min(len(cossim[-1]), 3)]
-    ret = []
-    # print(relevant)
-    for r in relevant:
-        # print(r)
-        if r != len(cossim[-1]) - 1:
-            ret.append(list(tweets_data.keys())[r])
-    data = {
-        "matches": ret,
-        "similarity": cossim[-1][relevant].tolist()
-    }
-    list_words.pop()
-    return json.dumps(data)
 
 # we should also print out tweets/popularity
 
 
-@app.route("/")
+@ app.route("/")
 def home():
     return render_template('base.html', title="sample html")
 
 
-@app.route("/episodes")
+@ app.route("/episodes")
 def episodes_search():
     text = request.args.get("title")
-    return cossim_search(text)
+    # print(svd_cos(text, docs, wcnt, dcn, itp))
+    return json.dumps(svd_cos(text, docs, wcnt, dcn, itp))
 
 
 if 'DB_NAME' not in os.environ:
--- backend/helpers/similarity.py
+++ backend/helpers/similarity.py
@@ -1,5 +1,79 @@
 """collection of functions helpful for similarity"""
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import normalize
+import numpy as np
+
+
+def load_data():
+    """load the data from the json and numpy file"""
+    pass
+
+# def closest_words(word_in, words_representation_in, k=10):
+#     if word_in not in vocab:
+#         return "Not in vocab."
+#     sims = words_representation_in.dot(
+#         words_representation_in[vocab[word_in], :])
+#     asort = np.argsort(-sims)[:k+1]
+#     return [(index_to_word[i], sims[i]) for i in asort[1:]]
+
+
+# Xnorm = X.transpose().toarray()
+# Xnorm = normalize(Xnorm)
+
+# word = 'hate'
+# print("Using SVD:")
+# for w, sim in closest_words(word, words_compressed_normed):
+#     try:
+#         print("{}, {:.3f}".format(w, sim))
+#     except:
+#         print("word not found")
+# print()
+
+
+# # this is basically the same cosine similarity code that we used before, just with some changes to
+# # the returned output format to let us print out the documents in a sensible way
+
+
+# def closest_projects(project_index_in, project_repr_in, k=5):
+#     sims = project_repr_in.dot(project_repr_in[project_index_in, :])
+#     asort = np.argsort(-sims)[:k+1]
+#     return [(itp[i], sims[i]) for i in asort[1:]]
+
+
+# def closest_projects_to_word(word_in, k=5):
+#     if word_in not in vocab:
+#         return "Not in vocab."
+#     sims = docs_compressed_normed.dot(
+#         words_compressed_normed[vocab[word_in], :])
+#     asort = np.argsort(-sims)[:k+1]
+#     return [(i, itp[i], sims[i]) for i in asort[1:]]
+
+
+# # for i, proj, sim in closest_projects_to_word("florida"):
+# #     print("({}, {}, {:.4f}".format(i, proj, sim))
+
+
+# svd cossine sim for top 10
+def svd_cos(query, docs, words_compressed_normed_transpose, docs_compressed_normed, itp, k=5, max_df=0.7, min_df=3):
+    vectorizer = TfidfVectorizer(stop_words='english', max_df=max_df,
+                                 min_df=min_df)
+    vectorizer.fit_transform(docs)
+    query_tfidf = vectorizer.transform([query]).toarray()
+    query_vec = normalize(
+        np.dot(query_tfidf, words_compressed_normed_transpose)).squeeze()
+    sims = docs_compressed_normed.dot(query_vec)
+    asort = np.argsort(-sims)[:k+1]
+    # print([itp[str(i)] for i in asort[1:]])
+    # print([i for i in asort[1:]])
+    # print([sims[i] for i in asort[1:]])
+    record = {
+        "index": [int(i) for i in asort[1:]],
+        "matches": [itp[str(i)] for i in asort[1:]],
+        "similarity": [sims[i] for i in asort[1:]]
+    }
+    return record
+
 
 def cosine_sim(query, tfidf):
     """basic similarity measure between a query and tfidf matrix"""
