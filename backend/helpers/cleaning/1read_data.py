5e09ffbcedcd7565ff0a3b271acf848eda2b781a
Kevin Cui <kc734@cornell.edu>
2024-04-14 22:35:40 -0400

    updated to remove duplicate tweets

--- backend/helpers/cleaning/1read_data.py
+++ backend/helpers/cleaning/1read_data.py
@@ -5,12 +5,13 @@ df = pd.read_csv('data/tweets/scraped.csv', keep_default_na=False)
 data = {}
 df_clean = df.to_dict(orient='records')
 a = 0
+seenContent = set()
 for i in range(0, len(df_clean)):
     if True:
         a += 1
         if df_clean[i]['Name'] not in data:
             data[df_clean[i]['Name']] = []
-        if df_clean[i]['Content'] != "":
+        if df_clean[i]['Content'] != "" and df_clean[i]['Content'] not in seenContent:
             data[df_clean[i]['Name']].append(
                 {'Handle': df_clean[i]['Handle'],
                  'Image': df_clean[i]['Profile Image'],
@@ -18,6 +19,7 @@ for i in range(0, len(df_clean)):
                  'Retweets': df_clean[i]['Retweets'],
                  'Likes': df_clean[i]['Likes'],
                  'Content': df_clean[i]['Content']})
+            seenContent.add(df_clean[i]['Content'])
 
 with open('data/tweets/raw.json', 'w') as outfile:
     json.dump(data, outfile)
