3f8fcbbd3faf22a447bed3d3b12afe562e6cdb07
Kevin Cui <kc734@cornell.edu>
2024-04-18 22:17:24 -0400

    working now

--- backend/app.py
+++ backend/app.py
@@ -53,15 +53,17 @@ CORS(app)
 
 # we should also print out tweets/popularity
 
+
 def normalize_name(name):
-    pattern = re.compile(r'^(Rep\.|Senator|Speaker|Archive:)\s+', re.IGNORECASE)
+    pattern = re.compile(
+        r'^(Rep\.|Senator|Speaker|Archive:)\s+', re.IGNORECASE)
 
     while True:
         new_name = pattern.sub('', name)
         if new_name == name:
             break
         name = new_name
-    
+
     name = re.sub(r'\b[A-Z]\.\s*', '', name)
     name = re.sub(r'\s+(Jr\.|Sr\.)', '', name)
 
@@ -70,22 +72,29 @@ def normalize_name(name):
 
 def update_json(input_json):
     people_data = json.loads(people_csv)
-    people_dict = {normalize_name(person['name']): person for person in people_data}
-    
+    people_dict = {normalize_name(
+        person['name']): person for person in people_data}
+
     for _, match in enumerate(input_json['matches']):
         normalized_match = normalize_name(match)
         if normalized_match in people_dict:
             person = people_dict[normalized_match]
-            input_json['country'] = input_json.get('country', []) + [person['country']]
-            input_json['chamber'] = input_json.get('chamber', []) + [person['chamber']]
-            input_json['party'] = input_json.get('party', []) + [person['party']]
-            input_json['region'] = input_json.get('region', []) + [person['region']]
+            input_json['country'] = input_json.get(
+                'country', []) + [person['country']]
+            input_json['chamber'] = input_json.get(
+                'chamber', []) + [person['chamber']]
+            input_json['party'] = input_json.get(
+                'party', []) + [person['party']]
+            input_json['region'] = input_json.get(
+                'region', []) + [person['region']]
         else:
-            input_json['country'] = input_json.get('country', []) + ["Not Found"]
-            input_json['chamber'] = input_json.get('chamber', []) + ["Not Found"]
+            input_json['country'] = input_json.get(
+                'country', []) + ["Not Found"]
+            input_json['chamber'] = input_json.get(
+                'chamber', []) + ["Not Found"]
             input_json['party'] = input_json.get('party', []) + ["Not Found"]
             input_json['region'] = input_json.get('region', []) + ["Not Found"]
-    
+
     return input_json
 
 
@@ -103,10 +112,10 @@ def episodes_search():
         record = svd_cos(text, docs, tweets, wcnt, dcn, itp)
     if record is None:
         record = boolean_search(text, itp, tweets, thresh=0)
-        
+
     if record is not None:
         record = update_json(record)
-    # print(record)
+    print(record)
     return json.dumps(record)
 
 
